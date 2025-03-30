2f7b954164f933941efad90ded305e75efab6dda
Runyu (Ryan) Zhang <fallacy1977@gmail.com>
2025-03-30 15:39:32 -0400

    modified:   findBlockNonce.py

--- findBlockNonce.py
+++ findBlockNonce.py
@@ -8,20 +8,35 @@ def mine_block(k, prev_hash, transactions):
     """
         k - Number of trailing zeros in the binary representation (integer)
         prev_hash - the hash of the previous block (bytes)
-        rand_lines - a set of "transactions," i.e., data to be included in this block (list of strings)
+        transactions - a set of "transactions," i.e., data to be included in this block (list of strings)
 
-        Complete this function to find a nonce such that 
-        sha256( prev_hash + rand_lines + nonce )
+        Finds a nonce such that 
+        sha256( prev_hash + transactions + nonce )
         has k trailing zeros in its *binary* representation
     """
     if not isinstance(k, int) or k < 0:
         print("mine_block expects positive integer")
         return b'\x00'
 
-    # TODO your code to find a nonce here
+    # Combine prev_hash and transactions into a single byte string
+    block_data = prev_hash + ''.join(transactions).encode('utf-8')
 
-    assert isinstance(nonce, bytes), 'nonce should be of type bytes'
-    return nonce
+    nonce = 0
+    while True:
+        # Convert nonce to bytes
+        nonce_bytes = str(nonce).encode('utf-8')
+
+        # Compute the hash of the block data + nonce
+        hash_result = hashlib.sha256(block_data + nonce_bytes).digest()
+
+        # Convert the hash to binary and check the number of trailing zeros
+        binary_hash = bin(int.from_bytes(hash_result, byteorder='big'))
+        if binary_hash[-k:] == '0' * k:
+            assert isinstance(nonce_bytes, bytes), 'nonce should be of type bytes'
+            return nonce_bytes
+
+        # Increment nonce for the next attempt
+        nonce += 1
 
 
 def get_random_lines(filename, quantity):
